d5be441e10776085dda989f9d1ab67a0d179cf4f
Ambika Tiwari <108768418+buffeinstein@users.noreply.github.com>
2025-09-03 16:30:44 -0700

    typo

--- 217_Contains_Duplicate.cpp
+++ 217_Contains_Duplicate.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen; 
+        unordered_set<int> seen;
         // unordered_set is just a hashtable that inserts items in only once
         // takes in num, creates a hash for it, and then places it into the bucket for that hash if its not been inserted before 
-        for (int num : nums): 
+        for (int num : nums){
             if (seen.count(num))
                 return true; 
-            seen.insert(num)
+            seen.insert(num);
     } 
     return false;    
     }
