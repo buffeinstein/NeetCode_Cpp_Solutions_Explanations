02e54f28c822111c3bfc13ad0f429c8176539e1b
Ambika Tiwari <108768418+buffeinstein@users.noreply.github.com>
2025-09-03 16:42:11 -0700

    Update 217_Contains_Duplicate.cpp

--- 217_Contains_Duplicate.cpp
+++ 217_Contains_Duplicate.cpp
@@ -6,9 +6,15 @@ public:
         // takes in num, creates a hash for it, and then places it into the bucket for that hash if its not been inserted before 
         for (int num : nums){
             if (seen.count(num))
+                // expected O(1), as it simply has to compute the hash for num again, and then check whether num is in the bucket
+                // worst case O(n), in the extremely low probability that all items landed in the same bucket 
                 return true; 
             seen.insert(num);
+            // ammortized expected O(1) 
+            // if load factor N/M exceeds max_load_factor, the table resizes (often doubling bucket count).
     } 
+    // O(n) expected runtime 
+    // O(n) memory 
     return false;    
     }
 };
